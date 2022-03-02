7f9c3e8269a5ae849558e91df594627fc2b6988f
Steven Saluri <43164747+stevsal@users.noreply.github.com>
2022-03-02 18:31:59 +0200

    user info requests

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
-const http = require('http');
+const express = require('express')
+const jsonServer = require('json-server')
+const app = express()
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+const port = 3000
+const userRequests = require('./getUserInfo.js')
 
-const hostname = '127.0.0.1';
-const port = 3000;
+server.use(middlewares)
+server.use(router)
+server.listen(3001, () => {
+  console.log('JSON Server is running');
+})
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+app.use(express.json())
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+app.post('/user/info/:header?', async (req,res) => {
+  var user = req.body.user//req.params.user;
+  var request_uuid = req.body.request_uuid// req.params.requuid
+  var body = req.body//req.json({body: req.body})
+  res.json(await userRequests.userInfo(user, request_uuid))
+})
+
+app.post('/user/balance/:header?', async (req,res) => {
+  var user = req.body.user
+  var request_uuid = req.body.request_uuid
+  var token = req.body.token
+  var game_code = req.body.game_code
+  res.json(await userRequests.userBalance(user, token, request_uuid, game_code))
+})
