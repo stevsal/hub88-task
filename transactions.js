b9ebfd1053be41711c88390888ce7d3839f9753c
Steven Saluri <43164747+stevsal@users.noreply.github.com>
2022-03-10 13:22:35 +0200

    Added some comments

--- transactions.js
+++ transactions.js
@@ -3,12 +3,16 @@ const userRequests = require('./getUserInfo.js')
 const dbActions = require('./dbActions.js')
 
 const transactionWin = async (data) => {
+    // Checking if win transactions has happened with that uuid and type
     var dbData = await getDBdata(data.user, data.transaction_uuid, data.type)
     var id
     var currency
     var newBalance = 0
     if (dbData == undefined) {
+        // No Transaction found with that uuid inserting the transaction
         await dbActions.insertIntoDB(data)
+        // Getting user info from DB and calculating new balance
+        // (Should have put checks here to verify correct amount and currency)
         await userRequests.getUserInfofromDB(data.user, data.request_uuid).then(
             response => {
                 newBalance = {
@@ -20,7 +24,9 @@ const transactionWin = async (data) => {
                 console.log(newBalance);
             }
         )
+        // Push new balance into DB
         await dbActions.changeUserInDB(newBalance,id)
+        // Return new balance to the request
         return {
             user: data.user,
             status: "RS_OK",
@@ -29,6 +35,7 @@ const transactionWin = async (data) => {
             balance: newBalance.balance
         }
     } else {
+        // If there is a transaction with that uuid return error to the request
         console.log(dbData);
         console.log('is there');
         return {
@@ -46,16 +53,20 @@ const transactionRollback = async function (data) {
     var amount
     var id
     var currency
-    var newBalance = 0
+    var newBalance
     var dbData
+    // Check DB for transaction that needs to be rolledback
     dbData = await getDBdata(data.user, refid, data.type)
     if (dbData != undefined) {
+        // If found get the amount that needs to be deducted or given back 
         amount = dbData.amount
         console.log('is there');
+        // Get userinfo from DB
         await userRequests.getUserInfofromDB(data.user,refid).then(
             res => {
                 id = res.id
                 if (dbData.type == 'win') {
+                    // If win we take away from balance
                     newBalance = {
                         balance: res.balance - amount,
                         request_uuid: data.request_uuid
@@ -70,6 +81,7 @@ const transactionRollback = async function (data) {
                 currency = res.currency
             }
         )
+        // Change users balance and insert transaction into DB
         dbActions.changeUserInDB(newBalance, id)
         dbActions.insertIntoDB(data)
         return {
@@ -80,6 +92,7 @@ const transactionRollback = async function (data) {
             balance: newBalance.balance
         }
     } else {
+        // If nothing to rollback return
         console.log('is not there');
         return {
             user: data.user,
@@ -95,6 +108,7 @@ const transactionBet = async function (data) {
     var id
     var newBalance
     if (data.amount < 0) {
+        // Check if amount is a wacky number like -1
         console.log('bad amount');
         return {
             user: data.user,
@@ -102,11 +116,14 @@ const transactionBet = async function (data) {
             request_uuid: data.request_uuid,
         }
     }
+    // Checking if transaction has already happened
     dbData = await getDBdata(data.user,data.transaction_uuid,data.type)
     if (dbData == undefined) {
        var userData = await userRequests.getUserInfofromDB(data.user,data.transaction_uuid)
+       //Send user data and transaction data to checks
         newBalance = await checkBetData(data,userData)
         if (newBalance.status == "RS_OK") {
+            // If checks ok then change user balance in db and insert transaction into db
             delete newBalance.status
             dbActions.changeUserInDB(newBalance, userData.id)
             dbActions.insertIntoDB(data)
